46e70e59c58583d8d5c38ffcfac37a640a4791b5
Jakob Petsovits <jpetso@topologyinc.com>
2015-06-06 15:59:43 -0400

    Add license headers to all the source files (Apache License 2.0).

--- src/bonefish/serialization/json_msgpack_sax.hpp
+++ src/bonefish/serialization/json_msgpack_sax.hpp
@@ -1,28 +1,29 @@
-//
-// Copyright (C) 2015 Topology LP
-//
-// Adapted from:
-//
-// MessagePack for C++ (release 1.0.1)
-// Copyright (C) 2008-2009 FURUHASHI Sadayuki
-// (Note jpetso: I'm sure there's some additional copyright but none as been
-// added by upstream contributors. As long as it's all Apache, it's all good.)
-//
-// https://github.com/xpol/xchange, commit c823ecd6f1bf42a6469a00b297a770e46a23d8e1 (2015-03-27)
-// Copyright (C) 2014-2015 Xpol Wan
-//
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
-//
-//        http://www.apache.org/licenses/LICENSE-2.0
-//
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
-//
+/**
+ *  Copyright (C) 2015 Topology LP
+ *
+ *  Adapted from:
+ *
+ *  MessagePack for C++ (release 1.0.1)
+ *  Copyright (C) 2008-2009 FURUHASHI Sadayuki
+ *  (Note jpetso: I'm sure there's some additional copyright but none has been
+ *  explicitly added by upstream contributors. Since it's all Apache, it's all good.)
+ *
+ *  https://github.com/xpol/xchange, commit c823ecd6f1bf42a6469a00b297a770e46a23d8e1 (2015-03-27)
+ *  Copyright (C) 2014-2015 Xpol Wan
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 #ifndef BONEFISH_SERIALIZATION_JSON_MSGPACK_SAX_HPP
 #define BONEFISH_SERIALIZATION_JSON_MSGPACK_SAX_HPP
 
