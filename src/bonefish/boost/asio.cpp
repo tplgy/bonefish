c503a023d317def7bbad9a394a34877388e8978a
Jakob Petsovits <jpetso@topologyinc.com>
2015-08-26 17:08:25 -0400

    Remove asio_source option.
    Using header-only asio gives us consistently 100 KB or more of
    binary size savings, while also simplifying build complexities
    and the use of bonefish as a library. For shared builds, we don't
    want the BOOST_ASIO_DYN_LINK option anyway because it ties us to
    asio's ABI, which has been declared as not stable at all.

--- src/bonefish/boost/asio.cpp
+++
@@ -1,4 +0,0 @@
-// This is the Include Boost.Asio source code if not used as header-only library.
-#if defined(BOOST_ASIO_DYN_LINK) || defined(BOOST_ASIO_SEPARATE_COMPILATION)
-#include <boost/asio/impl/src.hpp>
-#endif
