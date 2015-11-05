28407528a83bd6c2f5da9b86b71ae88687bb18e3
Omair Baskanderi <obaskanderi@topologyinc.com>
2015-11-05 15:14:02 -0500

    Fix build: handle case if RT Library is not found

--- cmake/Modules/FindRT.cmake
+++ cmake/Modules/FindRT.cmake
@@ -9,11 +9,13 @@ if(RT_USE_STATIC_LIBS)
     set(CMAKE_FIND_LIBRARY_SUFFIXES ${_rt_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES})
 endif()
 
-set(RT_LIBRARIES ${RT_LIBRARY})
-
 # Handle the QUIETLY and REQUIRED arguments and set RT_FOUND to true
 # if all listed variables are true.
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(RT DEFAULT_MSG RT_LIBRARY)
+
+if (RT_FOUND)
+	set(RT_LIBRARIES ${RT_LIBRARY})
+endif()
 
 mark_as_advanced(RT_LIBRARIES)
